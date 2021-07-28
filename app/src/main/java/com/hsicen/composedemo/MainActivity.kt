c5d17b129eccbc78e1167e4d3724885100038c70
hsicen <codinghuang@163.com>
2021-07-28 11:16:19 +0800

    compose text

--- app/src/main/java/com/hsicen/composedemo/MainActivity.kt
+++ app/src/main/java/com/hsicen/composedemo/MainActivity.kt
@@ -3,8 +3,6 @@ package com.hsicen.composedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,12 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Text("Hello Compose")
         }
     }
 }
