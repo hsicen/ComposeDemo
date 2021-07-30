6ae0e09d90e3cce8320a7aea9bcf4783c45d14af
hsicen <codinghuang@163.com>
2021-07-30 18:23:19 +0800

    constraintLayout sample

--- app/src/main/java/com/hsicen/composedemo/MainActivity.kt
+++ app/src/main/java/com/hsicen/composedemo/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +21,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.hsicen.composedemo.ui.theme.ComposeDemoTheme
 
 /**
@@ -42,81 +44,158 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun NewStory() {
     MaterialTheme {
-        val typography = MaterialTheme.typography
-
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.header),
-                contentDescription = "",
-                modifier = Modifier
-                    .height(180.dp)
-                    .fillMaxWidth()
-                    .clickable(onClick = {})
-                    .clip(shape = RoundedCornerShape(8.dp)),
-                contentScale = ContentScale.Crop
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = "锄禾",
-                style = typography.h6,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(text = "锄禾日当午", style = typography.body2)
-            Text(text = "汗滴禾下土", style = typography.body2)
-            Text(text = "谁知盘中餐", style = typography.body2)
-            Text(text = "粒粒皆辛苦", style = typography.body2)
-
-            Text(
-                "Hello World",
-                modifier = Modifier
-                    .background(Color.Green)
-                    .padding(20.dp)
-            )
-
-            Box(
-                Modifier
-                    .background(Color.Blue)
-                    .size(50.dp)
-                    .padding(10.dp)
-            ) {
-                Box(
-                    Modifier
-                        .background(Color.Gray)
-                        .fillMaxSize()
-                )
-            }
-
-            Box {
-                Spacer(
-                    Modifier
-                        .matchParentSize()
-                        .background(Color.Green)
-                )
-                Text(
-                    text = "Hello Compose",
-                    Modifier.paddingFromBaseline(top = 32.dp)
-                )
-            }
-
-            //offset
-            Box(
-                Modifier
-                    .background(Color.Yellow)
-                    .size(150.dp, 70.dp)
-            ) {
-                Text(
-                    text = "Layout offset modifier sample",
-                    Modifier.offset(16.dp, 12.dp)
-                )
-            }
-
-
+            Poetic()
+            Padding()
+            BaseLine()
+            Offset()
+            FixedSize()
+            Weight()
+            Constrain()
+            ItemConstrain()
         }
+    }
+}
+
+@Composable
+fun Poetic() {
+    val typography = MaterialTheme.typography
+    Image(
+        painter = painterResource(id = R.drawable.header),
+        contentDescription = "",
+        modifier = Modifier
+            .height(180.dp)
+            .fillMaxWidth()
+            .clickable(onClick = {})
+            .clip(shape = RoundedCornerShape(8.dp)),
+        contentScale = ContentScale.Crop
+    )
+
+    Spacer(modifier = Modifier.height(16.dp))
+    Text(
+        text = "锄禾",
+        style = typography.h6,
+        maxLines = 2,
+        overflow = TextOverflow.Ellipsis
+    )
+    Text(text = "锄禾日当午", style = typography.body2)
+    Text(text = "汗滴禾下土", style = typography.body2)
+    Text(text = "谁知盘中餐", style = typography.body2)
+    Text(text = "粒粒皆辛苦", style = typography.body2)
+}
+
+@Composable
+fun Padding() {
+    Text(
+        "Hello World",
+        modifier = Modifier
+            .background(Color.Green)
+            .padding(20.dp)
+    )
+
+    Box(
+        Modifier
+            .background(Color.Blue)
+            .size(50.dp)
+            .padding(10.dp)
+    ) {
+        Box(
+            Modifier
+                .background(Color.Gray)
+                .fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun BaseLine() {
+    Box {
+        Spacer(
+            Modifier
+                .matchParentSize()
+                .background(Color.Green)
+        )
+        Text(
+            text = "Hello Compose",
+            Modifier.paddingFromBaseline(top = 32.dp)
+        )
+    }
+}
+
+@Composable
+fun Offset() {
+    Box(
+        Modifier
+            .background(Color.Yellow)
+            .size(150.dp, 70.dp)
+    ) {
+        Text(
+            text = "Layout offset modifier sample",
+            Modifier.offset(16.dp, 12.dp)
+        )
+    }
+}
+
+@Composable
+fun FixedSize() {
+    Box {
+        Box(
+            Modifier
+                .size(90.dp, 150.dp)
+                .background(Color.Green)
+        ) {
+            Box(
+                Modifier
+                    .requiredSize(100.dp, 100.dp)
+                    .background(Color.Red)
+            )
+        }
+
+    }
+}
+
+@Composable
+fun Weight() {
+    Row(Modifier.fillMaxWidth()) {
+        Box(
+            modifier = Modifier
+                .weight(2f)
+                .height(50.dp)
+                .background(Color.Blue)
+        )
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(70.dp)
+                .background(Color.Yellow)
+        )
+    }
+}
+
+@Composable
+fun Constrain() {
+    BoxWithConstraints {
+        Text("My height[$minHeight,$maxHeight] and width[$minWidth,$maxWidth]")
+    }
+}
+
+@Composable
+fun ItemConstrain() {
+    ConstraintLayout {
+        val (button, text) = createRefs()
+
+        Button(onClick = {}, modifier = Modifier.constrainAs(button) {
+            top.linkTo(parent.top, margin = 16.dp)
+        }) {
+            Text(text = "Button")
+        }
+
+        Text(text = "Text", Modifier.constrainAs(text) {
+            top.linkTo(button.bottom, margin = 16.dp)
+        })
     }
 }
 
